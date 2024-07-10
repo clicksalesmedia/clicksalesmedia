202d6f0c38a3dde5465f31443b80d5dfd4bfd438
mounirActualMarketing <mounir@actual-mktg.com>
2024-07-10 07:00:41 -0700

    update gads tracking

--- src/app/ui/modalWebanimation.tsx
+++ src/app/ui/modalWebanimation.tsx
@@ -1,8 +1,8 @@
 import React, { useState, FunctionComponent, ChangeEvent, FormEvent } from 'react';
 import { Button, Label, Select, TextInput } from 'flowbite-react';
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from 'framer-motion';
 import Swal from 'sweetalert2';
-import { FiAlertCircle } from "react-icons/fi";
+import { FiAlertCircle } from 'react-icons/fi';
 
 interface FormServiceProps {
   isOpen: boolean;
@@ -13,23 +13,23 @@ interface FormServiceProps {
 declare global {
   interface Window {
     dataLayer: Array<Record<string, any>>;
+    gtag: (...args: any[]) => void;
   }
 }
 
 const FormService: FunctionComponent<FormServiceProps> = ({ isOpen, setIsOpen, buttonText }) => {
-  // Form state initialization
   const [form, setForm] = useState({
     name: '',
     company: '',
     website: '',
     mobile: '',
-    package: 'Premium', // Default service or could be empty string ''
+    package: 'Premium',
     email: '',
   });
 
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
-    setForm(prev => ({ ...prev, [name]: value }));
+    setForm((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
@@ -42,19 +42,17 @@ const FormService: FunctionComponent<FormServiceProps> = ({ isOpen, setIsOpen, b
         },
         body: JSON.stringify(form),
       });
-  
+
       if (response.ok) {
-        // SweetAlert2 for successful submission
         Swal.fire({
           icon: 'success',
           title: 'Thank you for completing the form.',
           text: 'Please wait while our customer service team reaches out to you as soon as possible.',
           background: '#272727',
           confirmButtonColor: '#C3A177',
-          confirmButtonText: 'Okay'
+          confirmButtonText: 'Okay',
         });
-        setIsOpen(false); // Close modal on success
-        // Reset form state
+        setIsOpen(false);
         setForm({
           name: '',
           company: '',
@@ -63,43 +61,45 @@ const FormService: FunctionComponent<FormServiceProps> = ({ isOpen, setIsOpen, b
           package: 'Premium',
           email: '',
         });
+
+        // Push event to Google Analytics and Google Ads
+        window.gtag('event', 'form_submission', {
+          event_category: 'Form',
+          event_label: 'Service Form',
+        });
+
+        // Push form data to dataLayer
+        window.dataLayer.push({
+          event: 'animationFormSubmission',
+          formData: {
+            name: form.name,
+            company: form.company,
+            website: form.website,
+            mobile: form.mobile,
+            package: form.package,
+            email: form.email,
+          },
+        });
+
       } else {
-        // SweetAlert2 for failure
         Swal.fire({
           icon: 'error',
           title: 'Submission Failed',
           text: 'Failed to submit lead. Please try again.',
           confirmButtonColor: '#d33',
-          confirmButtonText: 'Retry'
+          confirmButtonText: 'Retry',
         });
       }
     } catch (error) {
       console.error('Failed to submit lead', error);
-      // SweetAlert2 for catch error
       Swal.fire({
         icon: 'error',
         title: 'Error',
         text: 'An error occurred. Please try again.',
         confirmButtonColor: '#d33',
-        confirmButtonText: 'Retry'
+        confirmButtonText: 'Retry',
       });
     }
-
-// Ensure dataLayer is initialized
-window.dataLayer = window.dataLayer || [];
-
-// Push the form data
-window.dataLayer.push({
-  event: 'formSubmission',
-  formData: {
-    name: form.name,
-    company: form.company,
-    website: form.website,
-    mobile: form.mobile,
-    package: form.package,
-    email: form.email
-  }
-});
   };
 
   return (
@@ -113,9 +113,9 @@ window.dataLayer.push({
           className="bg-primaryColor/20 backdrop-blur p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer"
         >
           <motion.div
-            initial={{ scale: 0, rotate: "12.5deg" }}
-            animate={{ scale: 1, rotate: "0deg" }}
-            exit={{ scale: 0, rotate: "0deg" }}
+            initial={{ scale: 0, rotate: '12.5deg' }}
+            animate={{ scale: 1, rotate: '0deg' }}
+            exit={{ scale: 0, rotate: '0deg' }}
             onClick={(e) => e.stopPropagation()}
             className="bg-gradient-to-br from-[#222222] to-primaryColor text-white p-6 rounded-lg w-full max-w-lg shadow-xl cursor-default relative overflow-hidden"
           >
@@ -127,44 +127,96 @@ window.dataLayer.push({
               <form className="space-y-4" onSubmit={handleSubmit}>
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="base" value="Name" className='text-whiteColor'/>
+                    <Label htmlFor="base" value="Name" className="text-whiteColor" />
                   </div>
-                  <TextInput type="text" sizing="md" id="name" name="name" value={form.name} onChange={handleChange} placeholder="Your Full Name" style={{ background: '#222222',color:'#C3A177', borderColor: '#C3A177', borderRadius:1}}/>
+                  <TextInput
+                    type="text"
+                    sizing="md"
+                    id="name"
+                    name="name"
+                    value={form.name}
+                    onChange={handleChange}
+                    placeholder="Your Full Name"
+                    style={{ background: '#222222', color: '#C3A177', borderColor: '#C3A177', borderRadius: 1 }}
+                  />
                 </div>
 
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="email2" value="Your email" className='text-whiteColor'/>
+                    <Label htmlFor="email2" value="Your email" className="text-whiteColor" />
                   </div>
-                  <TextInput id="email" type="email"  name="email" value={form.email} onChange={handleChange} placeholder="name@example.com" required shadow style={{ background: '#222222',color:'#C3A177', borderColor: '#C3A177', borderRadius:1}}/>
+                  <TextInput
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={form.email}
+                    onChange={handleChange}
+                    placeholder="name@example.com"
+                    required
+                    shadow
+                    style={{ background: '#222222', color: '#C3A177', borderColor: '#C3A177', borderRadius: 1 }}
+                  />
                 </div>
 
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="base" value="Company" className='text-whiteColor'/>
+                    <Label htmlFor="base" value="Company" className="text-whiteColor" />
                   </div>
-                  <TextInput type="text" sizing="md" id="company" name="company" value={form.company} onChange={handleChange} placeholder="Your Company Name" style={{ background: '#222222',color:'#C3A177', borderColor: '#C3A177', borderRadius:1}}/>
+                  <TextInput
+                    type="text"
+                    sizing="md"
+                    id="company"
+                    name="company"
+                    value={form.company}
+                    onChange={handleChange}
+                    placeholder="Your Company Name"
+                    style={{ background: '#222222', color: '#C3A177', borderColor: '#C3A177', borderRadius: 1 }}
+                  />
                 </div>
 
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="base" value="Website" className='text-whiteColor'/>
+                    <Label htmlFor="base" value="Website" className="text-whiteColor" />
                   </div>
-                  <TextInput type="text" sizing="md" id="website" name="website" value={form.website} onChange={handleChange} placeholder="www.yourwebsite.com" style={{ background: '#222222',color:'#C3A177', borderColor: '#C3A177', borderRadius:1}}/>
+                  <TextInput
+                    type="text"
+                    sizing="md"
+                    id="website"
+                    name="website"
+                    value={form.website}
+                    onChange={handleChange}
+                    placeholder="www.yourwebsite.com"
+                    style={{ background: '#222222', color: '#C3A177', borderColor: '#C3A177', borderRadius: 1 }}
+                  />
                 </div>
 
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="base" value="Mobile" className='text-whiteColor'/>
+                    <Label htmlFor="base" value="Mobile" className="text-whiteColor" />
                   </div>
-                  <TextInput id="mobile" type="text" sizing="md" name="mobile" value={form.mobile} onChange={handleChange} placeholder="Your Mobile Number" style={{ background: '#222222',color:'#C3A177', borderColor: '#C3A177', borderRadius:1}}/>
+                  <TextInput
+                    id="mobile"
+                    type="text"
+                    sizing="md"
+                    name="mobile"
+                    value={form.mobile}
+                    onChange={handleChange}
+                    placeholder="Your Mobile Number"
+                    style={{ background: '#222222', color: '#C3A177', borderColor: '#C3A177', borderRadius: 1 }}
+                  />
                 </div>
 
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="Package" value="Select Package" className="text-left text-white"/>
+                    <Label htmlFor="Package" value="Select Package" className="text-left text-white" />
                   </div>
-                  <Select id="package" name="package" value={form.package} onChange={handleChange} style={{ background: '#222222',color:'#C3A177', borderColor: '#C3A177', borderRadius:1}}>
+                  <Select
+                    id="package"
+                    name="package"
+                    value={form.package}
+                    onChange={handleChange}
+                    style={{ background: '#222222', color: '#C3A177', borderColor: '#C3A177', borderRadius: 1 }}
+                  >
                     <option value="Basic">Basic</option>
                     <option value="Standard">Standard</option>
                     <option value="Premium">Premium</option>
@@ -175,7 +227,7 @@ window.dataLayer.push({
                   type="submit"
                   className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-md text-sm font-medium rounded-xs text-white bg-secondaryColor hover:bg-[#b89469] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#b89469]"
                 >
-                  Power Up Your Project
+                  {buttonText}
                 </Button>
               </form>
             </div>
